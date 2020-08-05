59c8fcf91e586663c2258611db4e5775411f6ab4
Darko Stojanovski <darkoluis@gmail.com>
2020-08-05 12:32:09 -0400

    Updates

---
+++ precache-manifest.445a3612ea4f86d4c06b4da7a66c091e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d3430d5a48b07bc74dce0cec1884b14",
+    "url": "/Employee-Directory_MERN/index.html"
+  },
+  {
+    "revision": "af34b675ea003ffe6387",
+    "url": "/Employee-Directory_MERN/static/css/main.8b5b3d4a.chunk.css"
+  },
+  {
+    "revision": "1d14c5b80f756b5c85b5",
+    "url": "/Employee-Directory_MERN/static/js/2.6d9f8b2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory_MERN/static/js/2.6d9f8b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af34b675ea003ffe6387",
+    "url": "/Employee-Directory_MERN/static/js/main.26d146ac.chunk.js"
+  },
+  {
+    "revision": "5afdc5909576e1a20367",
+    "url": "/Employee-Directory_MERN/static/js/runtime-main.17ce251a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Employee-Directory_MERN/static/media/logo.5d5d9eef.svg"
+  }
+]);
